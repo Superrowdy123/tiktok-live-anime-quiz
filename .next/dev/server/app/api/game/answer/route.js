4793a07f5c38aa4b5a561be0408f48c9f15a8c51
Superrowdy123 <136205294+Superrowdy123@users.noreply.github.com>
2026-06-15 22:39:08 +0300

    Initial commit without node_modules

--- .next/dev/server/app/api/game/answer/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/game/answer/route.js")
-R.c("server/chunks/src_app_api_game_answer_route_ts_0prxni_._.js")
-R.c("server/chunks/node_modules_next_0kr5ho7._.js")
-R.c("server/chunks/[root-of-the-server]__0xuz5bc._.js")
-R.c("server/chunks/_next-internal_server_app_api_game_answer_route_actions_0nht0bd.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/game/answer/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/game/answer/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/app/api/game/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/game/route.js")
-R.c("server/chunks/_0qbky0o._.js")
-R.c("server/chunks/node_modules_next_0qnsgk6._.js")
-R.c("server/chunks/[root-of-the-server]__0joshdm._.js")
-R.c("server/chunks/_next-internal_server_app_api_game_route_actions_045q27n.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/game/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/game/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
